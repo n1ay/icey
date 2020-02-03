2c364447779dc4b41a04d775318c55064a0a91bc
kamil <kammmilkos@gmail.com>
2020-02-03 21:30:19 +0100

    add profiles

--- src/main/java/com/github/n1ay/icey/Controller.java
+++ src/main/java/com/github/n1ay/icey/Controller.java
@@ -1,12 +1,21 @@
 package com.github.n1ay.icey;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class Controller {
+    public String indexMsg;
+
+    @Autowired
+    public void setIndexMsg(String indexMsg) {
+        this.indexMsg = indexMsg;
+    }
+
     @RequestMapping("/")
     public String index() {
-        return "Hello World!";
+        return indexMsg;
     }
 }
---
+++ src/main/java/com/github/n1ay/icey/Home.java
@@ -0,0 +1,20 @@
+package com.github.n1ay.icey;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Home {
+    @Bean
+    @Profile("dev")
+    public String testIndex() {
+        return "test";
+    }
+
+    @Bean
+    @Profile("prod")
+    String index() {
+        return "Hello World!";
+    }
+}
