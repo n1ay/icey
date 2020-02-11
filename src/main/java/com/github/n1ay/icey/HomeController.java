898d56449699b95c144d2e52461205c6206066c2
kamil <kammmilkos@gmail.com>
2020-02-11 21:01:49 +0100

    add basic thymeleaf templates

--- src/main/java/com/github/n1ay/icey/HomeController.java
+++ src/main/java/com/github/n1ay/icey/HomeController.java
@@ -1,12 +1,11 @@
 package com.github.n1ay.icey;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Controller {
+public class HomeController {
     public String indexMsg;
 
     @Autowired
--- src/main/java/com/github/n1ay/icey/Launcher.java
+++ src/main/java/com/github/n1ay/icey/Launcher.java
@@ -2,11 +2,12 @@ package com.github.n1ay.icey;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import java.util.Collections;
 
 @SpringBootApplication
-public class Launcher {
+public class Launcher extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Launcher.class);
         app.setDefaultProperties(Collections.singletonMap("server.port", 8880));
