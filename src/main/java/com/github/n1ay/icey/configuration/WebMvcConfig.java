6d9d8bd0e662fdfe0bf8399a165df5e89b0092be
kamil <kammmilkos@gmail.com>
2020-02-27 21:53:06 +0100

    add UserDetailsService implementation

--- src/main/java/com/github/n1ay/icey/configuration/WebMvcConfig.java
+++ src/main/java/com/github/n1ay/icey/configuration/WebMvcConfig.java
@@ -6,11 +6,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
                 .addResourceLocations("classpath:/images/")
                 .setCachePeriod(0);
     }
-
 }
---
+++ src/main/java/com/github/n1ay/icey/service/UserDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package com.github.n1ay.icey.service;
+
+import com.github.n1ay.icey.data.model.User;
+import com.github.n1ay.icey.data.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException(email);
+        }
+        return fromUser(user);
+    }
+
+    private UserDetails fromUser(User user) {
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRoles().stream().map(Enum::toString).toArray(String[]::new))
+                .build();
+    }
+}
