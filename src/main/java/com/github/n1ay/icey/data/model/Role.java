d25c3d650cc687b1a0341e1c773298b6f76048c5
kamil <kammmilkos@gmail.com>
2020-05-05 22:30:24 +0200

    add default role sets for users

--- src/main/java/com/github/n1ay/icey/data/model/Role.java
+++ src/main/java/com/github/n1ay/icey/data/model/Role.java
@@ -1,12 +1,28 @@
 package com.github.n1ay.icey.data.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public enum Role {
     USER("USER"),
     ADMIN("ADMIN");
 
-    private String name;
+    private final String name;
 
     Role(String name) {
         this.name = name;
+    }
+
+    public static Set<Role> getUserRoleSet() {
+        Set<Role> roleSet = new HashSet<>(1);
+        roleSet.add(USER);
+        return roleSet;
+    }
+
+    public static Set<Role> getAdminRoleSet() {
+        Set<Role> roleSet = new HashSet<>(2);
+        roleSet.add(ADMIN);
+        roleSet.add(USER);
+        return roleSet;
     }
 }
