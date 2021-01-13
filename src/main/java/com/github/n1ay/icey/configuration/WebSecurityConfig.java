7cda31a02bfd1cc831d6dfd416317deb8abb01e2
kamil <kammmilkos@gmail.com>
2021-01-13 22:11:46 +0100

    chore: config security for logging out

--- src/main/java/com/github/n1ay/icey/configuration/WebSecurityConfig.java
+++ src/main/java/com/github/n1ay/icey/configuration/WebSecurityConfig.java
@@ -2,16 +2,12 @@ package com.github.n1ay.icey.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -23,22 +19,28 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/greeting").permitAll()
-                .antMatchers("/images/**").permitAll()
+                .antMatchers("/login",
+                        "/register",
+                        "/greeting",
+                        "/images/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
-            .formLogin(withDefaults())
-            .logout(withDefaults())
-            .httpBasic();
+            .formLogin()
+                .permitAll()
+                .and()
+            .logout()
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login?logout")
+                .permitAll()
+                .and()
+            .httpBasic()
+                .disable();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService);
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
